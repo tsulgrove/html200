96a7720483079660d6b426feddcd509788fd2b9a
Thomas Sulgrove <tsulgrov@amazon.com>
2023-04-27 09:27:40 -0600

    challenge 1

---
+++ lesson02/calculator.js
@@ -0,0 +1,20 @@
+let result = 0;
+
+const one = 1;
+const two = 2;
+const three = 3;
+
+result = one + two;
+console.log(result);
+
+result = one - two;
+console.log(result);
+
+result = two * three;
+console.log(result);
+
+result = three / two;
+console.log(result);
+
+result = three % two;
+console.log(result);
